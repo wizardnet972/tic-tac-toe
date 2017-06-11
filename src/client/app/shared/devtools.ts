8ddbaaaa743b1ce03ce2ac9f78a6da229f496d87
Wizardnet972 <wizardnet972@gmail.com>
2017-06-11 21:46:44 +0300

    remove old version

--- src/client/app/shared/devtools.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'dev-tools',
-  template: `
-    <ngrx-store-log-monitor toggleCommand="ctrl-t" positionCommand="ctrl-m"></ngrx-store-log-monitor>
-  `
-})
-export class DevtoolsComponent { }
